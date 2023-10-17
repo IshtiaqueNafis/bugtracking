c9d9bc51e31ca49b6cad2f3a908284de865fdf2e
nafu <nafu22@gmail.com>
2023-10-16 22:23:28 -0400

    show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingIssueDetailPage = () => {
+    return (
+        <div>
+            Loading Issue Detail
+        </div>
+    );
+};
+
+export default LoadingIssueDetailPage;
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingNewIssue = () => {
+    return (
+        <div>
+            Loading new issue
+        </div>
+    );
+};
+
+export default LoadingNewIssue;
